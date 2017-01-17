f463cbe24d08733ea230ae818aef229c726407b1
Chris <chris@greenhost.nl>
2017-01-17 15:04:34 +0100

    make cache simple again

--- util/cache.py
+++ util/cache.py
@@ -1,24 +1,8 @@
 """
-    Simple lightweight caching class without dependencies or fancy features.
+    Simple lightweight caching class without third-party dependencies or fancy
+    features.
 """
 # pylint: disable=invalid-name
 
-
-class cache(dict):
-    """
-    Cache decorator for caching function output based on (keyword) arguments.
-    """
-    def __init__(self, function):
-        self.function = function
-        super(cache, self).__init__()
-        self.__doc__ = function.__doc__
-
-    def __call__(self, *args, **kwargs):
-        return self.wrapper(*args, **kwargs)
-
-    def __missing__(self, key):
-        result = self[key] = self.function(*key[0], **dict(key[1]))
-        return result
-
-    def wrapper(self, *args, **kwargs):
-        return self[args, tuple(kwargs.items())]
+from functools import lru_cache
+cache = lru_cache(None)
