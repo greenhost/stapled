ff357608322b8726a21ea64f8b6a4c5d077ea395
Maarten de Waard <maarten@greenhost.nl>
2017-02-01 15:55:11 +0100

    install ocspd.service when running setup.py install or pip install

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@
 """
 Python setuptools script for ``ocspd`` application.
 """
+import os
+import shutil
 from setuptools import setup
 from setuptools import find_packages
+from setuptools.command.install import install
 # pylint: disable=invalid-name
 
 version = '0.1'
@@ -12,7 +15,7 @@ install_requires = [
     'certvalidator>=0.11.1',
     'ocspbuilder>=0.10.2',
     'oscrypto>=0.17.2',
-    'python-daemon==1.5.5',
+    'python-daemon>=2',
     'requests>=2.4.3',
     'future>=0.15.0',
     'pylru>=1.0.9'
@@ -28,6 +31,37 @@ long_description = (
     "Update OCSP staples from CA's and store the result so "
     "they can be served to clients."
 )
+
+
+class CustomInstallCommand(install):
+    """
+    Installs systemd service to /lib/systemd/system/ocspd.service. Note that
+    this is not installed when installing with --editable or setup.py develop.
+    """
+
+    SERVICE_FILENAME = 'ocspd.service'
+    SERVICE_FILE = os.path.join(os.getcwd(), 'scripts', SERVICE_FILENAME)
+    SERVICE_DESTINATION_DIR = os.path.join('/lib', 'systemd', 'system')
+
+    def run(self):
+        """
+        Installs and then copies the service file to the systemd directory
+        """
+        if not os.path.exists(self.SERVICE_DESTINATION_DIR):
+            os.makedirs(self.SERVICE_DESTINATION_DIR)
+        install.run(self)
+        print("Installing ocspd.service")
+        service_dest = os.path.join(
+            self.SERVICE_DESTINATION_DIR,
+            self.SERVICE_FILENAME)
+        try:
+            shutil.copy(self.SERVICE_FILE, service_dest)
+        except IOError as exc:
+            if exc.errno == 13:
+                print("WARNING! systemd service was not installed due to "
+                      "permission problems")
+            else:
+                raise
 
 setup(
     name='ocspd',
@@ -67,5 +101,6 @@ setup(
         'console_scripts': [
             'ocspd = ocspd.__main__:init'
         ]
-    }
+    },
+    cmdclass={'install': CustomInstallCommand},
 )
