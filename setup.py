04428aad17ab7cbd9e45768c581df77549ec1953
Chris Snijder <chris@greenhost.nl>
2017-11-23 15:00:17 +0100

    Try without pip stuff.

--- setup.py
+++ setup.py
@@ -28,10 +28,10 @@ setup(
         'python-daemon>=1.5.5',
         'future>=0.15.0',
         'configargparse>=0.10.0',
-        'asn1crypto>=0.23.0',
-        'certvalidator>=0.11.1',
-        'ocspbuilder>=0.10.2',
-        'oscrypto>=0.18.0'
+        # 'asn1crypto>=0.23.0',
+        # 'certvalidator>=0.11.1',
+        # 'ocspbuilder>=0.10.2',
+        # 'oscrypto>=0.18.0'
     ],
     extras_require={
         'docs': [
